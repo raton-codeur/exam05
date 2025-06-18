7a195426a03103f9f5db191334257fc5a5ed633f
Quentin Hauuy <quentinhauuy@MacBook-Pro-de-Quentin.local>
2025-06-18 14:02:55 +0200

    blabla

---
+++ 00/Warlock.cpp
@@ -0,0 +1,48 @@
+#include "Warlock.hpp"
+
+Warlock::Warlock()
+{}
+
+Warlock::Warlock(const Warlock& other)
+{
+	(void)other;
+}
+
+Warlock& Warlock::operator=(const Warlock& other)
+{
+	(void)other;
+	return *this;
+}
+
+Warlock::~Warlock()
+{
+	std::cout << _name << ": My job here is done!\n";
+}
+
+const std::string& Warlock::getName() const
+{
+	return _name;
+}
+
+const std::string& Warlock::getTitle() const
+{
+	return _title;
+}
+
+
+void Warlock::setTitle(const std::string& title)
+{
+	_title = title;
+}
+
+Warlock::Warlock(const std::string& name, const std::string& title)
+{
+	_name = name;
+	_title = title;
+	std::cout << _name << ": This looks like another boring day.\n";
+}
+
+void Warlock::introduce() const
+{
+	std::cout << _name << " I am " << _name << ", " << _title << "!\n";
+}
